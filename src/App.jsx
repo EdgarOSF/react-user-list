e2a815bf77143f424d2c38425a76a1e1bfc441e3
EdgarOSF <edgaromarsf@gmail.com>
2022-05-11 12:38:21 -0500

    JSON-SEVER

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,5 @@
 import UserList from './components/UserList';
 
-const USERS = [
-  {
-    username: 'edgar',
-    name: 'Edgar Omar',
-    active: true,
-    role: 'teacher',
-  },
-  {
-    username: 'erika',
-    name: 'Erika Lisset',
-    active: true,
-    role: 'teacher',
-  },
-  {
-    username: 'mayi',
-    name: 'Mayanin de R',
-    active: false,
-    role: 'student',
-  },
-  {
-    username: 'enrique',
-    name: 'Enrique gonzalez',
-    active: false,
-    role: 'student',
-  },
-  {
-    username: 'javier',
-    name: 'Javier López',
-    active: true,
-    role: 'other',
-  },
-];
-
 const App = () => <UserList initialUsers={USERS} />;
 
 export default App;
